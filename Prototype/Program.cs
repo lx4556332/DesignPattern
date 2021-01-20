bcc74b002978299b5027c2a38073d3ff05df0673
luoxiang <luoxiang@cdzh.net>
2021-01-20 10:41:57 +0800

    1、单例模式

---
+++ Prototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = Signleton.GetInstance();
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(instance.GetDateTimeNow().ToString());
+                Console.WriteLine($"======={ instance.GetHashCode()}=======");
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prototype/Signleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Signleton
+    {
+        private static volatile Signleton instance = null;
+
+        private static object lockHelper = new Object();
+
+        private Signleton() { }
+
+
+        public static Signleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockHelper)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Signleton();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// 获取时间
+        /// </summary>
+        /// <returns></returns>
+        public  DateTime GetDateTimeNow()
+        {
+            return DateTime.Now;
+        }
+    }
+}
